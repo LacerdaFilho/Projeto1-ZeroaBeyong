ee056bdd0b1b41bb2ab7e9524804aa0b581983b1
Luciano Elizio <86981029+LacerdaFilho@users.noreply.github.com>
2021-09-18 12:28:16 -0300

    MessageCard.vue

---
+++ MessageCard.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-card outlined class="ma-3" :class="message.name == 'Eu' ? 'purple lighten-2' : 'teal accent-4'">
+        <v-card-subtitle class="font-weight-bold"> {{message.name == 'Eu' ? 'Eu' : firstName}} </v-card-subtitle>
+        <v-card-text>{{message.text}}</v-card-text>
+    </v-card>
+</template>
